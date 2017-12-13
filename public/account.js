87235b64f0682277d8c2528e1f5f6ec6c0c9cad0
musitive <dallinfrank@gmail.com>
2017-12-13 00:03:12 -0700

    fdsa

--- public/account.js
+++ public/account.js
@@ -21,7 +21,7 @@ angular.module('fakeNews', [])
     
     $scope.posts = [];
     $scope.getAll = function() {
-        return $http.get('/headlines').success(function(data){
+        return $http.get('/userHeadlines').success(function(data){
             console.log(data); 
             angular.copy(data, $scope.posts);
             });
